8be1435f139cbc33fad342017b581123b4582ec5
William Reeder <whhriv@gmail.com>
2023-12-05 15:13:23 -0800

    forgot to initialize API

--- app/__init__.py
+++ app/__init__.py
@@ -11,8 +11,8 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-# app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
 
 login = LoginManager()
 login.login_view = 'login' 
@@ -23,7 +23,7 @@ migrate = Migrate(app, db)
 # db.create_all()
 
 
-#from app.blueprints.api import api
-#app.register_blueprint(api)
+from app.blueprints.api import api
+app.register_blueprint(api)
 
 from . import routes, models, forms
